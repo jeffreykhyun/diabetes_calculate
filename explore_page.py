04ebaecce6bc3331937813176099625d3123f44b
jeffreykhyun <98427500+no1cdib@users.noreply.github.com>
2023-07-19 01:24:10 -0700

    first commit

---
+++ explore_page.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def recategorize_smoking(smoking_status):
+    if smoking_status in ['never', 'No Info']:
+        return 'non-smoker'
+    elif smoking_status == 'current':
+        return 'current'
+    elif smoking_status in ['ever', 'former', 'not current']:
+        return 'past_smoker'
+    
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("diabetes_prediction_dataset.csv")
+    df = df.drop_duplicates()
+    df = df[df['gender'] != 'Other']
+    df['smoking_history'] = df['smoking_history'].apply(recategorize_smoking)
+    return df
+
+df = load_data()
+
+def show_explore_page():
+    st.title("Explore Variables of Diabetes Risk")
+    fig1, ax1 = plt.subplots()
+    sns.boxplot(x='diabetes', y='age', data=df, ax=ax1, palette=['green', 'red'])
+    ax1.set_title('Age and Diabetes')
+    st.write("""#### Age and Diabetes """)
+    st.pyplot(fig1)
+
+    # Plot 2: Gender and Diabetes
+    fig2, ax2 = plt.subplots()
+    sns.countplot(x='gender', hue='diabetes', data=df, ax=ax2, palette=['green', 'red'])
+    ax2.set_title('Gender and Diabetes')
+    st.write("""#### Gender and Diabetes """)
+    st.pyplot(fig2)
+
+    # Plot 3: HbA1c levels and Diabetes
+    fig3, ax3 = plt.subplots()
+    sns.boxplot(x='diabetes', y='HbA1c_level', data=df, ax=ax3, palette=['green', 'red'])
+    ax3.set_title('HbA1c levels and Diabetes')
+    st.write("""#### HbA1c and Diabetes """)
+    st.pyplot(fig3)
+
+    # Plot 4: Blood Glucose Levels and Diabetes
+    fig4, ax4 = plt.subplots()
+    sns.boxplot(x='diabetes', y='blood_glucose_level', data=df, ax=ax4, palette=['green', 'red'])
+    ax4.set_title('Blood Glucose Levels and Diabetes')
+    st.write("""#### Blood Glucose Levels and Diabetes """)
+    st.pyplot(fig4)
+
+    fig5, ax5 = plt.subplots()
+    sns.boxplot(x='diabetes', y='bmi', hue='gender', data=df, ax=ax5, palette={'Female': 'pink', 'Male': 'aqua'})
+    ax5.set_title('BMI Distribution by Diabetes Diagnosis and Gender')
+    st.write("""#### BMI Distribution by Diabetes Diagnosis and Gender """)
+    st.pyplot(fig5)
+
+    fig6, ax6 = plt.subplots()
+    sns.boxplot(x='diabetes', y='age', hue='gender', data=df, ax=ax6, palette={'Female': 'pink', 'Male': 'aqua'})
+    ax6.set_title('Age Distribution by Diabetes Diagnosis and Gender')
+    st.write("""#### Age Distribution by Diabetes Diagnosis and Gender """)
+    st.pyplot(fig6)
+
+   
---
+++ predict_page.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+def load_model(): 
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+model = data['model']
+gender = data['gender']
+age = data['age']
+hypertension = data['hypertension']
+heart_disease = data['heart_disease']
+smoking_history = data['smoking_history']
+bmi = data['bmi']
+HbA1c_level = data['HbA1c_level']
+blood_glucose_level = data['blood_glucose_level']
+
+def show_predict_page():
+    st.title("Diabetes Risk Predictor")
+
+    st.write("""### We need some information to predict diabetes """)
+
+    gender = (
+        "Male",
+        "Female"
+    )
+
+    smoking_history = (
+        "non-smoker",
+        "current",
+        "past_smoker"
+    )
+
+    gender = st.selectbox("gender", gender)
+    age = st.number_input("Enter your age", min_value=0, max_value=120, step=1)
+    hypertension = st.checkbox("Do you have hypertension? Check for Yes.")
+    hypertension_value = 1 if hypertension else 0
+    heart_disease = st.checkbox("Do you have heart disease? Check for Yes.")
+    heart_disease_value = 1 if hypertension else 0
+    smoking_history = st.selectbox("smoking_history", smoking_history)
+    bmi = st.number_input("Enter your bmi", min_value=0.0)
+    HbA1c_level = st.number_input("Enter your HbA1c level", min_value=0.0)
+    blood_glucose_level = st.number_input("Enter your blood glucose level", min_value=0.0)
+
+    ok = st.button("Calculate Diabetes Risk")
+    if ok:
+        sample = {'gender': gender, 'age': age, 'hypertension': hypertension_value, 'heart_disease': heart_disease_value, 'smoking_history': smoking_history, 'bmi': bmi, 'HbA1c_level': HbA1c_level, 'blood_glucose_level': blood_glucose_level}
+        sample_df = pd.DataFrame(sample, index=[0])
+        diabetes_risk = model.predict(sample_df)
+        diagnosis = "Positive" if diabetes_risk[0] == 1 else "Negative"
+        st.subheader(f"The diagnosis for diabetes is {diagnosis}")
